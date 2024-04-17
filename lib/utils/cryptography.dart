554fcfc6384708416a1a23c4a547612da755f694
Tyler Suehr <tylersuehr@gmail.com>
2024-04-17 16:44:28 -0400

    Util function to constant-time compare binary

--- lib/utils/cryptography.dart
+++ lib/utils/cryptography.dart
@@ -12,3 +12,17 @@ Uint8List generateSecureEntropy(final int outputBytes) {
   random.seed(KeyParameter(Platform.instance.platformEntropySource().getBytes(32)));
   return random.nextBytes(outputBytes);
 }
+
+/// Computes equality of 2 byte arrays in constant-time.
+/// Always use this when comparing crypto sequences to thwart timing attacks.
+bool constantTimeComparison(final Uint8List a, final Uint8List b) {
+  final int n = a.length;
+  if (n != b.length) return false;
+
+  var mismatch = 0;
+  for (int i = 0; i < n; i++) {
+    mismatch |= a[i] ^ b[i];
+  }
+
+  return mismatch == 0;
+}
