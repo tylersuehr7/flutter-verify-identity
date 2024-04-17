ddd539dd635dbdea2cc26601f4e2029e9340342c
Tyler Suehr <tylersuehr@gmail.com>
2024-04-17 16:46:23 -0400

    Convenient mem utilities

--- lib/utils/cryptography.dart
+++ lib/utils/cryptography.dart
@@ -26,3 +26,60 @@ bool constantTimeComparison(final Uint8List a, final Uint8List b) {
 
   return mismatch == 0;
 }
+
+/// Quick data copy from src array to another.
+///
+/// [src] the source array to be copied from
+/// [srcOffset] the offset from which to start reading src
+/// [srcLength] the length of data to read from src
+/// [dest] the destination array to be written to
+/// [destOffset] the offset from which to start writing dest
+///
+/// [throws] RangeError if dest not large enough to be written into
+void arrayCopy(
+  final Uint8List src,
+  final int srcOffset,
+  final int srcLength,
+  final Uint8List dest,
+  final int destOffset,
+) {
+  final int iterations = srcOffset + srcLength;
+
+  int i = srcOffset;
+  int j = destOffset;
+
+  for (; i < iterations; i++, j++) {
+    dest[j] = src[i];
+  }
+}
+
+/// Quick data concatenation of multiple arrays.
+///
+/// [data] the arrays to be concatenated
+/// [returns] the concatenated array
+Uint8List arrayConcat(final List<Uint8List> data) {
+  int totalLength = 0;
+  for (final Uint8List array in data) {
+    totalLength += array.length;
+  }
+
+  final Uint8List buffer = Uint8List(totalLength);
+
+  int offset = 0;
+  for (final Uint8List array in data) {
+    arrayCopy(array, 0, array.length, buffer, offset);
+    offset += array.length;
+  }
+
+  return buffer;
+}
+
+/// Quick data memory reset for an array.
+///
+/// [src] the array whose contents to be reset
+/// [fill] the data to fill array with
+void arrayMemset(final Uint8List src, final int fill) {
+  for (int i = 0; i < src.length; i++) {
+    src[i] = fill;
+  }
+}
