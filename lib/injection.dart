253c2fa2d79cfc7d7b15f43873f85f51e4ec9ae2
Tyler Suehr <tylersuehr@gmail.com>
2024-04-17 15:48:14 -0400

    Injection

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,7 +1,17 @@
+import 'package:demo_verify/data/shared_preferences_verification_settings_repository.dart';
+import 'package:demo_verify/domain/repositories/verification_settings_repository.dart';
+import 'package:demo_verify/domain/services/verification_service.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final GetIt injector = GetIt.instance;
 
 Future<void> prepareDependencies() async {
+  final SharedPreferences preferences = await SharedPreferences.getInstance();
 
+  final verificationSettingsRepo = SharedPreferencesVerificationSettingsRepository(preferences);
+  final verificationService = VerificationService(verificationSettingsRepo);
+
+  injector.registerSingleton<IVerificationSettingsRepository>(verificationSettingsRepo);
+  injector.registerSingleton<VerificationService>(verificationService);
 }
